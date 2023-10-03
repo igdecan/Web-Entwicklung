9fb78fa6563569f6883198944611e38014e60f1e
igdecan <s_igde19@stud.hwr-berlin.de>
2023-10-03 22:50:22 +0200

    frontend-setup for sign_up and login

--- auth.py
+++ auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return redirect(url_for('app.todos'))
 
-@auth.route('sign_up')
+@auth.route('/sign_up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template('sign_up.html')
